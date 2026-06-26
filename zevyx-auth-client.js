15624a0c3a3edcaed0b6e386836ce25f50b6eab2
Ar0n1s_ <prchlikovazdenka65@gmail.com>
2026-06-26 12:15:23 +0000

    v2

--- zevyx-auth-client.js
+++ zevyx-auth-client.js
@@ -91,9 +91,17 @@ function field(name, label, type, placeholder, autocomplete) {
             }
             <div class="flex flex-col gap-2 items-center w-full"><div data-captcha></div></div>
             <div data-message class="hidden"></div>
-            <button class="inline-flex items-center justify-center rounded-md text-sm font-medium bg-primary text-primary-foreground hover:bg-primary/90 h-9 px-4 py-2 w-full" type="submit">
-              ${login ? "Přihlásit se" : "Zaregistrovat se"}
-            </button>
+<button class="inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium bg-primary text-primary-foreground hover:bg-primary/90 h-9 px-4 py-2 w-full" type="submit">
+  ${
+    login
+      ? `<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+           <path d="m22 2-7 20-4-9-9-4Z"/>
+           <path d="M22 2 11 13"/>
+         </svg>
+         <span>Přihlásit se</span>`
+      : "Zaregistrovat se"
+  }
+</button>
           </form>
         </div>
       </div>
