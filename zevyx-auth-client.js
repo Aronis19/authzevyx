1235899aec88ce73f2454b00c4d0720b905ccd04
Ar0n1s_ <prchlikovazdenka65@gmail.com>
2026-06-28 09:54:04 +0000

    Zvětšit ikony hodností

--- zevyx-auth-client.js
+++ zevyx-auth-client.js
@@ -310,7 +310,7 @@ const rankImage = rankImages[rankName.toLowerCase()];
           ? `<img
                src="${rankImage}"
                alt="${esc(rankName)}"
-               style="display:block;max-width:130px;max-height:28px;object-fit:contain"
+               style="display:block;max-width:180px;max-height:38px;object-fit:contain"
              >`
           : esc(rankName)
       }
